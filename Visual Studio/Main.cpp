2b48ed70acf76697efc685b4c79f7618b13814ad
Ryo Suzuki <reputeless+github@gmail.com>
2025-09-18 15:15:12 +0900

    初期状態

---
+++ Visual Studio/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include <print>
+#include "Point.hpp"
+#include "BinaryFileWriter.hpp"
+#include "BinaryFileReader.hpp"
+#include "Color.hpp"
+#include "Image.hpp"
+
+int main()
+{
+	std::println("こんにちは、セキュリティキャンプ！\n");
+
+	int a = 100;
+
+	std::println("C++{}", (a + 23));
+}
