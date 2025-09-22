4e89a40086bc9eb47b453e4fb659db9186859e9f
Ryo Suzuki <reputeless+github@gmail.com>
2025-09-22 16:27:20 +0900

    Update Color.hpp

--- 完成例/Color.hpp
+++ 完成例/Color.hpp
@@ -122,7 +122,7 @@ namespace mini
 		[[nodiscard]]
 		constexpr double grayscale() const noexcept
 		{
-			return (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+			return (0.299 * r) + (0.587 * g) + (0.114 * b);
 		}
 
 		/// @brief 出力ストリームに書き込みます。
